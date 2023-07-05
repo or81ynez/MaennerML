bfc98d6574dbc49f4cce491d46b7f724f5d9a25a
trammmd <tramduong1107@gmail.com>
2023-07-05 18:39:40 +0200

    Test stream data page

--- Main.py
+++ Main.py
@@ -6,14 +6,6 @@ import matplotlib as plt
 import seaborn as sns
 import time
 import numpy as np
-import dash
-from dash.dependencies import Output, Input
-from dash import dcc, html, dcc
-from datetime import datetime
-import json
-import plotly.graph_objs as go
-from collections import deque
-from flask import Flask, request
 
 #Functions 
 
@@ -90,78 +82,76 @@ def map_data(df):
     folium.PolyLine(coords, color="blue", weight=5.0).add_to(my_map)
     return my_map
 
+
 #Stream data
-server = Flask(__name__)
-app = dash.Dash(__name__, server=server)
+from flask import Flask, request
+import dash
+from dash.dependencies import Output, Input
+from dash import dcc, html, dcc
+from datetime import datetime
+import json
+import plotly.graph_objs as go
+from collections import deque
 
-MAX_DATA_POINTS = 1000
-UPDATE_FREQ_MS = 100
-
-time = deque(maxlen=MAX_DATA_POINTS)
-accel_x = deque(maxlen=MAX_DATA_POINTS)
-accel_y = deque(maxlen=MAX_DATA_POINTS)
-accel_z = deque(maxlen=MAX_DATA_POINTS)
-
-app.layout = html.Div(
-	[
-		dcc.Markdown(
-			children="""
-			# Live Sensor Readings
-			Streamed from Sensor Logger: tszheichoi.com/sensorlogger
-		"""
-		),
-		dcc.Graph(id="live_graph"),
-		dcc.Interval(id="counter", interval=UPDATE_FREQ_MS),
-	]
-)
-
-
-@app.callback(Output("live_graph", "figure"), Input("counter", "n_intervals"))
-def update_graph(_counter):
-	data = [
-		go.Scatter(x=list(time), y=list(d), name=name)
-		for d, name in zip([accel_x, accel_y, accel_z], ["X", "Y", "Z"])
-	]
-
-	graph = {
-		"data": data,
-		"layout": go.Layout(
-			{
-				"xaxis": {"type": "date"},
-				"yaxis": {"title": "Acceleration ms<sup>-2</sup>"},
-			}
-		),
-	}
-	if (
-		len(time) > 0
-	):  #  cannot adjust plot ranges until there is at least one data point
-		graph["layout"]["xaxis"]["range"] = [min(time), max(time)]
-		graph["layout"]["yaxis"]["range"] = [
-			min(accel_x + accel_y + accel_z),
-			max(accel_x + accel_y + accel_z),
-		]
-
-	return graph
-
-
-@server.route("/data", methods=["POST"])
-def data():  # listens to the data streamed from the sensor logger
-	if str(request.method) == "POST":
-		print(f'received data: {request.data}')
-		data = json.loads(request.data)
-		for d in data['payload']:
-			if (
-				d.get("name", None) == "accelerometer"
-			):  #  modify to access different sensors
-				ts = datetime.fromtimestamp(d["time"] / 1000000000)
-				if len(time) == 0 or ts > time[-1]:
-					time.append(ts)
-					# modify the following based on which sensor is accessed, log the raw json for guidance
-					accel_x.append(d["values"]["x"])
-					accel_y.append(d["values"]["y"])
-					accel_z.append(d["values"]["z"])
-	return "success"
-
-
-if __name__ == "__main__":
-	app.run_server(port=8000, host="0.0.0.0")
+#10.100.213.5
+def stream_data():
+   MAX_DATA_POINTS = 1000
+   UPDATE_FREQ_MS = 100
+   time = deque(maxlen=MAX_DATA_POINTS)
+   longitute = deque(maxlen=MAX_DATA_POINTS)
+   latitute = deque(maxlen=MAX_DATA_POINTS)
+   
+   app.layout = html.Div(
+         [
+               dcc.Markdown(
+         children="""
+         # Live Sensor Readings
+         Streamed from Sensor Logger: tszheichoi.com/sensorlogger
+         """
+         ),
+         dcc.Graph(id="live_graph"),
+         dcc.Interval(id="counter", interval=UPDATE_FREQ_MS),
+         ]
+         )
+   @app.callback(Output("live_graph", "figure"), Input("counter", "n_intervals"))
+   def update_graph(_counter):
+    data = [
+         go.Scatter(x=list(time), y=list(d), name=name)
+         for d, name in zip([longitute, latitute], ["X", "Y"])
+         ]
+    graph = {
+         "data": data,
+         "layout": go.Layout(
+         {
+              "xaxis": {"type": "date"},
+              "yaxis": {"title": "Acceleration ms<sup>-2</sup>"},
+              }
+              ),
+              }
+    if (
+         len(time) > 0
+         ):  #  cannot adjust plot ranges until there is at least one data point
+         graph["layout"]["xaxis"]["range"] = [min(time), max(time)]
+         graph["layout"]["yaxis"]["range"] = [
+              min(longitute + latitute),
+              max(longitute + latitute),
+              ]
+         return graph
+    @server.route("/data", methods=["POST"])
+    def data():  # listens to the data streamed from the sensor logger
+        if str(request.method) == "POST":
+            print(f'received data: {request.data}')
+            data = json.loads(request.data)
+            for d in data['payload']:
+                 if (
+                      d.get("name", None) == "accelerometer"
+                      ):  #  modify to access different sensors
+                      ts = datetime.fromtimestamp(d["time"] / 1000000000)
+                      if len(time) == 0 or ts > time[-1]:
+                        time.append(ts)
+                        # modify the following based on which sensor is accessed, log the raw json for guidance
+                        longitute.append(d["values"]["x"])
+                        latitute.append(d["values"]["y"])
+                        return "success"
+                      if __name__ == "__main__":
+                          app.run_server(port=8000, host="0.0.0.0")
